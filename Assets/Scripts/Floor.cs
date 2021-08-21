7206ff78fa63192b14c4353bf7fcfd7659db7b14
vkalev <vjk1883@gmail.com>
2021-08-21 12:37:27 -0400

    creating multiple floors + starting outer mesh

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -1,22 +1,24 @@
 using System.Collections.Generic;
-using System.Collections;
 using UnityEngine;
 
 public class Floor : MonoBehaviour
 {
-    public int SizeX = 40, SizeZ = 30;
+    private int SizeX, SizeZ;
     // public float stepDelay = 0.000000001f;
     MazeCell[,] maze;
-    // Stack<MazeCell> frontier;
     List<MazeCell> frontier;
     List<MazeCell> visited;
     public MazeCell cellPrefab;
     public Wall wallPrefab;
     public Passage passagePrefab;
+    private float FloorNumber;
 
-    public void Generate()
+    public void Generate(float floorNumber, int sizeX, int sizeZ)
     {
         // WaitForSeconds delay = new WaitForSeconds(stepDelay);
+        this.FloorNumber = floorNumber;
+        this.SizeX = sizeX;
+        this.SizeZ = sizeZ;
 		maze = new MazeCell[SizeX, SizeZ];
 		frontier = new List<MazeCell>();
         int randomX = Random.Range(0, SizeX);
@@ -48,30 +50,10 @@ public class Floor : MonoBehaviour
                 CreateWall(currentCell, neighbor, MazeDirections.Directions[neighborData[2]]);
             }
         } else {
-            CreateWall(currentCell, null, MazeDirections.Directions[neighborData[2]]);
+            CreatePassage(currentCell, null, MazeDirections.Directions[neighborData[2]]);
         }
     }
 
-    // public void Generate()
-    // {
-    //     maze = new MazeCell[SizeX, SizeZ];
-    //     frontier = new Stack<MazeCell>();
-    //     visited = new List<MazeCell>();
-    //     for (int x = 0; x < SizeX; x++) {
-    //         for (int z = 0; z < SizeZ; z++) {
-    //             MazeCell newCell = CreateCell(x, z);
-    //             MazeCell westNeighbor = null, southNeighbor = null;
-    //             if (x > 0) westNeighbor = maze[x-1, z];
-    //             if (z > 0) southNeighbor = maze[x, z-1];
-    //             CreateWall(newCell, westNeighbor, MazeDirection.West);
-    //             CreateWall(newCell, southNeighbor, MazeDirection.North);
-    //             if (x == SizeX-1) CreateWall(newCell, null, MazeDirection.East);
-    //             if (z == SizeZ-1) CreateWall(newCell, null, MazeDirection.North);
-    //         }
-    //     }
-    //     RandomizedDFS();
-    // }
-    
     private MazeCell CreateCell(int x, int z)
     {
         MazeCell newCell = Instantiate(cellPrefab) as MazeCell;
@@ -80,7 +62,7 @@ public class Floor : MonoBehaviour
         newCell.Z = z;
         newCell.name = "Cell " + x + ", " + z;
 		newCell.transform.parent = transform;
-		newCell.transform.localPosition = new Vector3(x - SizeX * 0.5f + 0.5f, 0f, z - SizeZ * 0.5f + 0.5f);
+		newCell.transform.localPosition = new Vector3(x - SizeX * 0.5f + 0.5f, FloorNumber, z - SizeZ * 0.5f + 0.5f);
         return newCell;
     }
 
@@ -89,8 +71,10 @@ public class Floor : MonoBehaviour
         if (cell.GetEdge(direction) == null) {
             Passage newPassage = Instantiate(passagePrefab) as Passage;
             newPassage.Initialize(cell, neighbor, direction);
-            newPassage = Instantiate(passagePrefab) as Passage;
-            newPassage.Initialize(neighbor, cell, direction.GetOpposite());
+            if (neighbor != null) {
+                newPassage = Instantiate(passagePrefab) as Passage;
+                newPassage.Initialize(neighbor, cell, direction.GetOpposite());
+            }
         }
     }
 
@@ -111,68 +95,42 @@ public class Floor : MonoBehaviour
         return x >= 0 && x < SizeX && z >= 0 && z < SizeZ;
     }
 
-    // void RandomizedDFS() 
-    // {
-    //     int randomX = Random.Range(0, SizeX);
-    //     int randomZ = Random.Range(0, SizeZ);
-    //     MazeCell initial = maze[randomX, randomZ];
-    //     // visited.Add(initial);
-    //     frontier.Push(initial);
+    public MazeCell[] GetMazeBorder (MazeDirection direction)
+    {
+        MazeCell[] border;
+        switch (direction)
+        {
+            case MazeDirection.North:
+                border = new MazeCell[SizeX];
+                for (int col = 0; col < SizeX; col++)
+                {
+                    border[col] = maze[col, 0];
+                }
+                return border;
+            
+            case MazeDirection.East:
+                border = new MazeCell[SizeZ];
+                for (int row = 0; row < SizeZ; row++)
+                {
+                    border[row] = maze[SizeX-1, row];
+                }
+                return border;
 
-    //     while (frontier.Count > 0) {
-    //         MazeCell cell = frontier.Pop();
-    //         // visited.Add(cell);
-    //         if (!visited.Contains(cell)) {
-    //             visited.Add(cell);
-    //             List<MazeCell> neighbors = GetNeighbors(cell.X, cell.Z);
-    //             if (neighbors.Count > 0) {
-    //                 int position = Random.Range(0, neighbors.Count);
-    //                 for (int i = 0; i < neighbors.Count; i++) {
-    //                     MazeCell currNeighbor = neighbors[i];
-    //                     CreatePassage(cell, currNeighbor, MazeDirections.Directions[i]);
-    //                     // visited.Add(currNeighbor);
-    //                     if (i != position && !frontier.Contains(currNeighbor)) frontier.Push(currNeighbor);
-    //                 }
-    //                 if (!frontier.Contains(neighbors[position])) frontier.Push(neighbors[position]);
-    //             }
-    //         }
-    //     }
-    // }
+            case MazeDirection.South:
+                border = new MazeCell[SizeX];
+                for (int col = 0; col < SizeX; col++)
+                {
+                    border[col] = maze[col, SizeZ-1];
+                }
+                return border;
 
-    // List<MazeCell> GetNeighbors(int x, int z)
-    // {
-    //     List<MazeCell> validNeighbors = new List<MazeCell>();
-    //     MazeCell northNeighbor = null, eastNeighbor = null, southNeighbor = null, westNeighbor = null;
-        
-    //     if (z < SizeZ-1) northNeighbor = maze[x, z+1];
-    //     if (x < SizeX-1) eastNeighbor = maze[x+1, z];
-    //     if (z > 0) southNeighbor = maze[x, z-1];
-    //     if (x > 0) westNeighbor = maze[x-1, z];
-
-    //     if (northNeighbor != null && !visited.Contains(northNeighbor)) {
-    //         validNeighbors.Add(northNeighbor);
-    //     } else {
-    //         CreateWall(maze[x, z], northNeighbor, MazeDirection.North);
-    //     }
-
-    //     if (eastNeighbor != null && !visited.Contains(eastNeighbor)) {
-    //         validNeighbors.Add(eastNeighbor);
-    //     } else {
-    //         CreateWall(maze[x, z], eastNeighbor, MazeDirection.East);
-    //     }
-
-    //     if (southNeighbor != null && !visited.Contains(southNeighbor)) {
-    //         validNeighbors.Add(southNeighbor);
-    //     } else {
-    //         CreateWall(maze[x, z], southNeighbor, MazeDirection.South);
-    //     }
-
-    //     if (westNeighbor != null && !visited.Contains(westNeighbor)) {
-    //         validNeighbors.Add(westNeighbor);
-    //     } else {
-    //         CreateWall(maze[x, z], westNeighbor, MazeDirection.West);
-    //     }
-
-    //     return validNeighbors;
-    // }
+            default:
+                border = new MazeCell[SizeZ];
+                for (int row = 0; row < SizeZ; row++)
+                {
+                    border[row] = maze[0, row];
+                }
+                return border;
+        }
+    }
 }
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    Mesh outerMesh;
     public Floor floorPrefab;
     private List<Floor> floors;
-    public int numFloors = 1;
+    public int floorSizeX = 70, floorSizeZ = 50;
+    public int numFloors = 3;
 
     void Start()
     {
@@ -14,9 +16,33 @@ public class Tower : MonoBehaviour
         for (int i = 0; i < numFloors; i++)
         {
             Floor currFloor = Instantiate(floorPrefab) as Floor;
-            currFloor.Generate();
+            currFloor.Generate((float) i * 5, floorSizeX, floorSizeZ);
+            floorSizeX -= 2;
+            floorSizeZ -= 2;
             // StartCoroutine(currFloor.Generate());
             floors.Add(currFloor);
+        }
+        GenerateOuterMesh();
+    }
+
+    public void GenerateOuterMesh()
+    {
+        for (int i = 0; i < numFloors-1; i++)
+        {
+            Floor currFloor = floors[i];
+            Floor topFloor = floors[i + 1];
+            foreach (MazeDirection direction in MazeDirections.Directions)
+            {
+                MazeCell[] currBorder = currFloor.GetMazeBorder(direction);
+                MazeCell[] topBorder = topFloor.GetMazeBorder(direction);
+                for (int c = 0; c < topBorder.Length; c++)
+                {
+                    if (c == 0 || c == topBorder.Length-1)
+                    {
+                        
+                    }
+                }
+            }
         }
     }
 
